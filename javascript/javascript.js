edfa0a68e0d600959693221861787ad3973c567a
Umar Adam <P15175838@LEC-ADMIN.dmu.ac.uk>
2017-12-06 10:56:58 +0000

    optimized images, edited css

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -40,3 +40,20 @@ let span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 } 
+
+/** accordian **/
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    }
+}
+
